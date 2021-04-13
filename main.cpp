0a96aeea8960325def12b1d07a611cc95b59b1d4
benfogiel <bfogiel@ucsb.edu>
2021-04-13 10:19:26 -0700

    new version

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
 	Transaction t;
 
 	while (input != 4){
-	cout << "Welcome to the transaction-chain application.... \n1) Add a transaction to the chain.\n2) Find balance of a person.\n3) Print the chain.\n4) Exit.\nWhich operation do you want to make? (1,2,3,4):" << endl;
+	cout << "Welcome to the transaction-chain application....\n1) Add a transaction to the chain.\n2) Find balance of a person.\n3) Print the chain.\n4) Exit.\nWhich operation do you want to make? (1,2,3,4):" << endl;
 	cin >> input;
 
 	if (input == 1) {
