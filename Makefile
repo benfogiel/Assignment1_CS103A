abd9e60d0eee147181b4ea59557216998b4debf2
benfogiel <bfogiel@ucsb.edu>
2021-04-13 13:49:16 -0700

    commit

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 main: main.o block.o
-	g++ main.o block.o -o main -lssl -lcrypto
+	g++ -g -Wall -std=c++11 main.o block.o -o main -lssl -lcrypto
+	#g++ -g -Wall -std=c++11 -o main main.cpp -L/usr/local/opt/openssl@1.1/lib -lcrypto
+
 
 main.o: main.cpp
 	g++ -c main.cpp
 
 block.o: block.cpp
 	g++ -c block.cpp
+
+clean:
+	rm -f main main.o block.o
--- main.cpp
+++ main.cpp
@@ -16,16 +16,15 @@ using namespace std;
 
 int main() {
 	int input = 0;
-
-// cout << sha256("6666661234567890_1") << endl;
-
 	Transaction t;
 
 	while (input != 4){
 	cout << "Welcome to the transaction-chain application....\n1) Add a transaction to the chain.\n2) Find balance of a person.\n3) Print the chain.\n4) Exit.\nWhich operation do you want to make? (1,2,3,4):" << endl;
 	cin >> input;
-
-	if (input == 1) {
+	
+	if(input != 1 && input != 2 && input != 3 && input != 4){
+		cout << "Please enter a valid option" << endl;
+	}else if (input == 1) {
 		int amount;
 		string sender, receiver;
 		cout << "Integer amount of money:" << endl;
@@ -36,20 +35,16 @@ int main() {
 		cin >> receiver;
 		
 		t.add(amount, sender, receiver);
-	}
-
-	if(input == 2){
+	}else if(input == 2){
 		string person;
 
 		cout << "Please type name (case-sensitive)" << endl;
 		cin >> person;
 
 		cout << t.GetBalance(person) << endl;
+	}else if(input == 3){ t.print();
+	}else if(input == 4){ break;
 	}
-
-	if(input == 3) t.print();
-
-	if(input == 4) break;
 	}
 	return 0;
 }
